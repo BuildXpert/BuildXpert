fc14a3a07769af8d39b8cc987616487cb978da0d
Chris2810 <christophercd960@gmail.com>
2025-03-10 23:41:17 -0600

    Removed unneccesary file sql.sh

--- sql.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-until /opt/mssql-tools/bin/sqlcmd -S SQL_Server -U sa -P "YourStrong!Passw0rd" -Q "SELECT 1"; do
-  echo"Waiting for SQL Server to start..."
-  sleep 5
-done
-echo"SQL Server is up. Starting backend..."
